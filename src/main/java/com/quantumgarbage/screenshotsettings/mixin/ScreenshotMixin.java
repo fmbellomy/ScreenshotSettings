585dd9772f625819af80883ef1554b6fccddf7b0
fmbellomy <fmbellomy@quantumgarbage.com>
2022-11-28 15:08:24 -0600

    rewrite @Overwrite mixin to multiple safer mixins

--- src/main/java/com/quantumgarbage/screenshotsettings/mixin/ScreenshotMixin.java
+++ src/main/java/com/quantumgarbage/screenshotsettings/mixin/ScreenshotMixin.java
@@ -5,68 +5,66 @@ import com.quantumgarbage.screenshotsettings.client.config.ScreenshotSettingsCon
 import com.quantumgarbage.screenshotsettings.util.FileNameTemplateProcessor;
 import com.quantumgarbage.screenshotsettings.util.PNGMetadataManipulator;
 import com.quantumgarbage.screenshotsettings.util.getters.GameMeta;
-import net.minecraft.client.gl.Framebuffer;
 import net.minecraft.client.texture.NativeImage;
 import net.minecraft.client.util.ScreenshotRecorder;
-import net.minecraft.text.ClickEvent;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
-import net.minecraft.util.Util;
-import org.jetbrains.annotations.Nullable;
-import org.slf4j.Logger;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.function.Consumer;
-
-import static net.minecraft.client.util.ScreenshotRecorder.takeScreenshot;
-
 
 @Mixin(ScreenshotRecorder.class)
 public class ScreenshotMixin {
+    @ModifyVariable(
+            method = "saveScreenshotInner",
+            at = @At(value = "STORE"),
+            index = 5)
+    private static File directoryInject(File f) {
+        return new File(ScreenshotSettingsConfig.INSTANCE.screenshotDirectory);
+    }
 
-    @Final
-    @Shadow
-    private static Logger LOGGER;
-    /**
-     * @author fmbellomy
-     * @reason i suck at mixins and this was the easiest way to do what i needed to do.
-     */
-    @Overwrite
-    private static void saveScreenshotInner(File gameDirectory, @Nullable String fileName, Framebuffer framebuffer, Consumer<Text> messageReceiver) {
-        NativeImage nativeImage = takeScreenshot(framebuffer);
-        String dir = ScreenshotSettingsConfig.INSTANCE.getScreenshotDirectory();
-        File file = new File(dir);
-        //noinspection ResultOfMethodCallIgnored
-        file.mkdir();
-        File file2;
+    @ModifyVariable(
+            method = "saveScreenshotInner",
+            argsOnly = true,
+            at = @At(value = "HEAD"),
+            index = 1
+    )
+    private static String filenameInject(String orig) {
         String filename = FileNameTemplateProcessor.format(ScreenshotSettingsConfig.INSTANCE.screenshotNamingSchema);
-        file2 = new File(file, filename);
-        file2.getParentFile().mkdirs();
-        int i = 1;
-        while (new File(file2 + ".png").exists()) {
-            ++i;
-            file2 = new File(file, filename + "_(" + i + ")");
+        // base filename
+        String filenameChecked = filename + ".png";
+        // check if another file with the given name already exists.
+        // if it does, add a number suffix.
+        for (int i = 1; Files.exists(Path.of(filenameChecked)); ++i) {
+            filenameChecked = filename + "_" + i + ".png";
         }
-        final File file3 = new File(file2 + ".png");
-        Util.getIoWorkerExecutor().execute(() -> {
-            try {
-                nativeImage.writeTo(file3);
-                Text text = Text.literal(file3.getName()).formatted(Formatting.UNDERLINE).styled((style) -> style.withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_FILE, file3.getAbsolutePath())));
-                messageReceiver.accept(Text.translatable("screenshot.success", text));
-                LOGGER.info("Screenshot successfully saved to " + file3.getAbsolutePath() + "!");
+        return filenameChecked;
+    }
 
-                PNGMetadataManipulator.attachMetadata(file3, GameMeta.getMetadata());
+    // inject into the lambda in Util.getServiceWorker.execute()
+    // this is to create any necessary subdirectories for the screenshots.
+    @Inject(
+            method = "method_1661",
+            at = @At(value = "HEAD"),
+            locals = LocalCapture.CAPTURE_FAILEXCEPTION
+    )
+    private static void createDirs(NativeImage _nativeImage, File file, Consumer<Text> _consumer, CallbackInfo _ci) {
+        file.getParentFile().mkdirs();
+    }
 
-            } catch (Exception var7) {
-                LOGGER.warn("Couldn't save screenshot", var7);
-                messageReceiver.accept(Text.translatable("screenshot.failure", var7.getMessage()));
-            } finally {
-                nativeImage.close();
-            }
-        });
+    @Inject(
+            method = "method_1661",
+            at = @At(value = "INVOKE", target = "Ljava/util/function/Consumer;accept(Ljava/lang/Object;)V", ordinal = 0),
+            locals = LocalCapture.CAPTURE_FAILEXCEPTION
+    )
+    private static void attachMetadata(NativeImage nativeImage, File file, Consumer<Text> consumer, CallbackInfo ci) {
+        PNGMetadataManipulator.attachMetadata(file, GameMeta.getMetadata());
     }
 }
